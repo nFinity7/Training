96290d0ffd94aa66b2b2e205ece8c7da3b3f254b
Sebastian <sebastian.juras@gmail.com>
2022-04-28 11:38:02 +0200

    Updates

---
+++ precache-manifest.66c6afb1bd4afad317a03d2f45d86f9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c63031ee26f362eba0d9402fd55fd70",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e2e19f53ac3cfa77827",
+    "url": "./static/css/2.148cc0e7.chunk.css"
+  },
+  {
+    "revision": "c8b7eeb24f6a3d2f5ffb",
+    "url": "./static/css/main.b30806fd.chunk.css"
+  },
+  {
+    "revision": "7e2e19f53ac3cfa77827",
+    "url": "./static/js/2.455fc1fa.chunk.js"
+  },
+  {
+    "revision": "f24b43d3afb04f6c9cea25be9ebdc537",
+    "url": "./static/js/2.455fc1fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b7eeb24f6a3d2f5ffb",
+    "url": "./static/js/main.0e07dbe4.chunk.js"
+  },
+  {
+    "revision": "a7dbfb0f6128d6fb4d78",
+    "url": "./static/js/runtime-main.bbd49688.js"
+  },
+  {
+    "revision": "a3b2d28b4a86cd0f7a962808fd67342f",
+    "url": "./static/media/marker.a3b2d28b.png"
+  }
+]);
